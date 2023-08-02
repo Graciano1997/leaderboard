abd17f1da4e5e945ce761c88fbc62ed40b4e9d88
graciano1997 <gracianomanuelhenrique@gmail.com>
2023-08-02 21:48:55 +0100

    Add webpack setup

---
+++ dist/index.bundle.js
@@ -0,0 +1,18 @@
+(self["webpackChunkworkflows"] = self["webpackChunkworkflows"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+/* eslint-disable  */
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  mode:'development',
+  devtool: 'inline-source-map',
+  devServer:{
+    static:'./dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean:true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
