76a1f73644df4cbe6513b67577b21679c9d5188a
graciano1997 <gracianomanuelhenrique@gmail.com>
2023-08-05 00:22:01 +0100

    add mobile website

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './index.css';
+import logo from './pictures/logo.png';
 import { createNewGame } from './modules/apiControler.js';
 import { initializeScoreOperationsListener, initializeScoreReader, refresAllListener } from './modules/scoreOperations.js';
 
@@ -6,3 +7,8 @@ createNewGame('Naruto_Heroes_Fight');
 initializeScoreReader();
 initializeScoreOperationsListener();
 refresAllListener();
+
+const myLogo = new Image();
+myLogo.src=logo;
+myLogo.classList.add('logo');
+document.querySelector('.logoContainer').append(myLogo);
