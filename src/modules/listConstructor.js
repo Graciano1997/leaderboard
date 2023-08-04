f2d533d192a0f8bb3b8ea55033ec25f1938d9b61
graciano1997 <gracianomanuelhenrique@gmail.com>
2023-08-04 18:28:52 +0100

    fix linter issue

--- src/modules/listConstructor.js
+++
@@ -1,7 +0,0 @@
-export default function listElementConstructor(score, even = null) {
-  const li = document.createElement('li');
-  li.classList.add('item');
-  if (even !== null) { li.classList.add('even'); }
-  li.textContent = `${score.user} : ${score.score}`;
-  return li;
-}
--- src/modules/scoreOperations.js
+++ src/modules/scoreOperations.js
@@ -1,5 +1,12 @@
 import * as Variable from './globalvar.js';
-import listElementConstructor from './listConstructor.js';
+
+const listElementConstructor = (score, even = null) => {
+  const li = document.createElement('li');
+  li.classList.add('item');
+  if (even !== null) { li.classList.add('even'); }
+  li.textContent = `${score.user} : ${score.score}`;
+  return li;
+};
 
 const cleanAll = () => {
   while (Variable.listContainer.firstChild) {
