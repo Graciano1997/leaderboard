cd60c2e0a025b6922b3143fc51760fa031f8f334
graciano1997 <gracianomanuelhenrique@gmail.com>
2023-08-06 00:40:45 +0100

    update favicon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './index.css';
 import './desktop.css';
+import icon from './pictures/icon.ico';
 import logo from './pictures/logo.png';
 import { createNewGame } from './modules/apiControler.js';
 import { initializeScoreOperationsListener, initializeScoreReader, refresAllListener } from './modules/scoreOperations.js';
@@ -11,6 +12,13 @@ initializeScoreOperationsListener();
 refresAllListener();
 themeFunctionListener();
 initializeBackground();
+
+const iconLink = document.createElement('link');
+iconLink.href = icon;
+iconLink.type = 'image/x-icon';
+iconLink.rel = 'shortcut icon';
+document.head.append(iconLink);
+
 const myLogo = new Image();
 myLogo.src = logo;
 myLogo.classList.add('logo');
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
         type: 'asset/resource',
       },
       {
