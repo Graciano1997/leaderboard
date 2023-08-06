fe1ade780c9d55e7b00251263744a01bc62b1ed9
graciano1997 <gracianomanuelhenrique@gmail.com>
2023-08-06 10:26:26 +0100

    Add og protocol

--- src/modules/metaData.js
+++ src/modules/metaData.js
@@ -3,24 +3,23 @@ import icon from '../pictures/icon.ico';
 import logo from '../pictures/logo.png';
 
 const metaHeaders = () => {
-  const iconLink = document.createElement('link');
-  iconLink.href = icon;
-  iconLink.type = 'image/x-icon';
-  iconLink.rel = 'shortcut icon';
+  // const iconLink = document.createElement('link');
+  // iconLink.href = icon;
+  // iconLink.type = 'image/x-icon';
+  // iconLink.rel = 'shortcut icon';
 
-  const metaTitle = document.createElement('meta');
-  metaTitle.property = 'og:title';
-  metaTitle.content = 'NARUTO LeadBoard Fighters';
+  // const metaTitle = document.createElement('meta');
+  // metaTitle.property = 'og:title';
+  // metaTitle.content = 'NARUTO LeadBoard Fighters';
 
-  const metaUrl = document.createElement('meta');
-  metaUrl.property = 'og:url';
-  metaUrl.content = 'https://graciano1997.github.io/leaderboard/dist/';
+  // const metaUrl = document.createElement('meta');
+  // metaUrl.property = 'og:url';
+  // metaUrl.content = 'https://graciano1997.github.io/leaderboard/dist/';
 
-  const metaImg = document.createElement('meta');
-  metaImg.property = 'og:image';
-  metaImg.content = '../pictures/NARUTOSAMA.jpg';
-
-  document.head.append(metaTitle, metaUrl, metaImg, iconLink);
+  // const metaImg = document.createElement('meta');
+  // metaImg.property = 'og:image';
+  // metaImg.content = '../pictures/NARUTOSAMA.jpg';
+  document.head.append(iconLink);
 };
 
 const setLogo = () => {
