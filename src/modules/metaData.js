af8ed63e8bf2f3ecf459bb696855cf5eae4cd49b
graciano1997 <gracianomanuelhenrique@gmail.com>
2023-08-06 23:44:33 +0100

    add music background

--- src/modules/metaData.js
+++ src/modules/metaData.js
@@ -3,10 +3,10 @@ import icon from '../pictures/icon.ico';
 import logo from '../pictures/logo.png';
 
 const metaHeaders = () => {
-   const iconLink = document.createElement('link');
-   iconLink.href = icon;
-   iconLink.type = 'image/x-icon';
-   iconLink.rel = 'shortcut icon';
+  const iconLink = document.createElement('link');
+  iconLink.href = icon;
+  iconLink.type = 'image/x-icon';
+  iconLink.rel = 'shortcut icon';
   document.head.append(iconLink);
 };
 
--- src/modules/theme.js
+++ src/modules/theme.js
@@ -1,4 +1,16 @@
 import * as Variable from './globalvar.js';
+import itachSong from '../music/itachi.mp3';
+import madaraSong from '../music/madara.mp3';
+
+const songs = [itachSong, madaraSong];
+const randomMusic = (musicArray) => {
+  const music = Math.floor(Math.random() * musicArray.length);
+  return musicArray[music];
+};
+
+const audio = new Audio(randomMusic(songs));
+audio.id = 'musicTheme';
+document.body.append(audio);
 
 const themeFunction = () => {
   const theme = ['url(../src/pictures/obito.jpg)', 'url(../src/pictures/madara.jpg)'];
@@ -21,6 +33,9 @@ const initializeBackground = () => {
   const theme = localStorage.getItem('NarutoTheme');
   Variable.bodyContainer.style.backgroundImage = theme;
   Variable.bodyContainer.style.backgroundSize = '100%';
+  document.querySelector('#musicTheme').currentTime = 0;
+  document.querySelector('#musicTheme').src = randomMusic(songs);
+  document.querySelector('#musicTheme').autoplay = true;
 };
 
 const themeFunctionListener = () => {
